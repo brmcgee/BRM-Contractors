0860f856b7ece366c4c7892aa794aaaa15195c2f
Brian McGee <92900596+brmcgee@users.noreply.github.com>
2023-02-13 19:55:37 -0500

    Add files via upload

--- blog-posts/js/app.js
+++ blog-posts/js/app.js
@@ -24,12 +24,36 @@ let blogData = [
         img2: "../assets/gallery/flooring2.jpg"
     },
     {
+        title: "Drywall board install and some extra framing",
+        date: "July 23, 2022",
+        author: "Brian McGee",
+        body: "We had good time here with general framing and windows as well as the electrical re-wire. We installed the drywall which we will finish later. Also added blocking for the TV. Someone will enjoy the games.",
+        img1: "../assets/gallery/drywall-hang1.jpg",
+        img2: "../assets/gallery/drywall-hang2.jpg"
+    },
+    {
+        title: "Drywall Finsihing",
+        date: "Aug 20, 2022",
+        author: "Brian McGee",
+        body: "He have returned from installting the original 30 sheets and are finishing this board on with a smooth finish. We have taken extra caution in protecting the floors. We are all sanded down and ready to for clean up here. This is beautiful but we wont be painting it. The homeowner will finish the rest.",
+        img1: "../assets/gallery/drywall-finish.jpg",
+        img2: "../assets/gallery/drywall-finish2.jpg"
+    },
+    {
         title: "Bathroom Remodel",
         date: "Dec 14, 2022",
         author: "Brian McGee",
         body: "This project was great! We completed it winter time. We removed everything down to the wood studs and installed all new insulation and fixtures. A 4-piece acrylic bathtub/shower combo kit. All new elctrical and lighting with bathroom exhaust. New smooth finished drywall surface freshly painted to our clients likings. I know its a bathroom but this will be a fun place.",
         img1: "../assets/gallery/bathroom1.jpg",
         img2: "../assets/gallery/bathroom2.jpg"
+    },
+    {
+        title: "OMG - 4 flights of Stairs Painting",
+        date: "July 11, 2022",
+        author: "Brian McGee",
+        body: "Wow when I got wind of this one. 4-flights of stairs to be painted here. Had to be careful not to get trapped on the top floor with wet paint... Well anyways it turned out awesome and paint lines were crisp. Another job DONE right!",
+        img1: "../assets/gallery/paint1.jpg",
+        img2: "../assets/gallery/paint2.jpg"
     }
 
 ];
