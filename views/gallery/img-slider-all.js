bde1fd58369a43944c99883f88490937b6c6cb94
Brian McGee <92900596+brmcgee@users.noreply.github.com>
2023-03-02 17:11:37 -0500

    Add files via upload

--- views/gallery/img-slider-all.js
+++ views/gallery/img-slider-all.js
@@ -1,14 +1,10 @@
 let wrapper = document.getElementById("pageWrap");
 let images = [];
-let imgData = data;
-let count = 0;
 let time = 2000;
 
-function showAllImages () {
-    console.log('hello')
 
-};
-
+//dom content creation per array
+// []
 function createImages(imgPath) {
     let heading = document.createElement('div');
     heading.id = "slide";
@@ -19,68 +15,85 @@ function createImages(imgPath) {
 
     let image = document.createElement('img');
     image.src = imgPath[0];
-    image.alt = "BRM";
+    image.alt = "BRM image";
     image.style.borderRadius = "10px"
     image.style.height = "80vh";
     image.style.width = "88vw";
     image.style.objectFit = "contain";
     heading.appendChild(image);
 
-
     let catTitle = document.createElement("h5");
-    catTitle.classList = "img-slider-title text-uppercase w-100"
+    catTitle.classList = "img-slider-title"
     catTitle.innerHTML = imgPath[1];
     heading.appendChild(catTitle);
 
     let catCategory = document.createElement("h3");
-    catCategory.classList = "img-slider-category text-uppercase w-100"
+    catCategory.classList = "img-slider-category"
     catCategory.innerHTML = imgPath[2];
     heading.appendChild(catCategory);
 };
 
-
 //creates array containing paths for images to display
-for (let i=0; i < data.length; i++){
-    let image1 = "../" + imgData[i].img1;
-    let image2 = "../" + imgData[i].img2;
-    let title = imgData[i].title;
-    let category = imgData[i].category;
+//pass in blog obect
+function getArray(obj){
 
-    images.push([image2, title, category]);
-    images.push([image1, title, category]);
+    for (let i=0; i < obj.length; i++){
+        let image1 = "../" + obj[i].img1;
+        let image2 = "../" + obj[i].img2;
+        let title = obj[i].title;
+        let category = obj[i].category;
+        images.push([image2, title, category]);
+        images.push([image1, title, category]);
+    }
+
+    return images;
 }
 
+//slideshow per img array query
+function runSlides(){
 
-images.forEach((img) => {
-    createImages(img);
-})
+    let slideshows = document.querySelectorAll('[data-component="slideshow"]');
 
+    slideshows.forEach(initSlideShow);
 
-
-// slideshow 
-let slideshows = document.querySelectorAll('[data-component="slideshow"]');
-
-slideshows.forEach(initSlideShow);
-
-function initSlideShow(slideshow){
-    let slides = document.querySelectorAll(`#${slideshow.id} [role="list"] .slide` )
-    let index = 0;
-
-    slides[index].classList.add('active');
-
-    setInterval(() => {
-        slides[index].classList.remove('active');
-        index++;
-
-        if (index === slides.length) {
-            index = 0;
-        }
+    function initSlideShow(slideshow){
+        let slides = document.querySelectorAll(`#${slideshow.id} [role="list"] .slide` )
+        let index = 0;
 
         slides[index].classList.add('active');
 
-    }, time);
+        setInterval(() => {
+            slides[index].classList.remove('active');
+            index++;
 
+            if (index === slides.length) {
+                index = 0;
+            }
+
+            slides[index].classList.add('active');
+
+        }, time);
+    };
 }
+
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    let imageArray = getArray(data);
+    imageArray.forEach((img) => {
+        createImages(img)
+    });
+
+    runSlides();
+});
+
+
+
+
+
+
 
 
 
