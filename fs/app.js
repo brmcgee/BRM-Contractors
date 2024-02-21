fe82203962418767ea87f3635a5d4400f86ec810
Brian McGee <92900596+brmcgee@users.noreply.github.com>
2024-02-20 20:51:29 -0500

    Add files via upload

---
+++ fs/app.js
@@ -0,0 +1,46 @@
+function elem (str) {
+    return document.querySelector(str);
+}
+
+
+
+let viewContainer = elem('#viewCont');
+let commentHTML = '';
+
+blogData.forEach(blog => {
+
+    let html = `
+    
+    <div class="card">
+        <div class="card-img-top">
+            <img src="${blog.img}" alt="">
+        </div>
+        <div class="card-body">
+            <div class="card-title">${blog.title}</div>
+            <div class="card-body">${blog.body}</div>
+        </div>
+    </div>
+    `;
+
+    blogComment.forEach(comment => {
+        console.log(comment.post_id + '   ' + blog.id);
+        if (comment.post_id == blog.id) {
+            commentHTML +=  `
+            <div class="comment">
+                <div class="comment-img-top">
+                    <img src="${comment.auth_avatar}" alt="">
+                </div>
+                <div class="comment-body">
+                    <div class="comment-title">${comment.author}</div>
+                    <div class="comment-body">${comment.comment}</div>
+                </div>
+            </div>        
+            `;
+        }
+
+    }) 
+
+    viewContainer.innerHTML += html;
+    viewContainer.innerHTML += commentHTML;
+});
+
