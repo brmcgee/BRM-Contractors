102d63d3ab7f5facc2dd5a7c58fcab107aaf709e
Brian McGee <92900596+brmcgee@users.noreply.github.com>
2024-04-16 22:35:28 -0400

    Add files via upload

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -3,7 +3,7 @@ function makeSmallBlog(blog) {
     let html= `
         <div class="col-6">
         <div class="card border-0">
-            <a href=""><img alt="" class="img-fluid rounded" src="${blog.img1}"></a>
+            <a href="https://boxcar.site/view.php?q=${blog.title}"><img alt="" class="img-fluid rounded" src="${blog.img1}"></a>
             <div class="card-body px-2">
                 <p class="text-primary badge bg-main text-light" href="">${blog.category}</p>
                 <h5 class="fw-semibold oswald" style="height:70px;"><a class="text-decoration-none text-dark" href="">${blog.title}</a></h5>
@@ -27,7 +27,7 @@ function makeBigBlog(blog) {
     let html= `
     
         <div class="card border-0">
-            <a href=""><img alt="" class="img-fluid rounded" src="${blog.img1}"></a>
+            <a href="https://boxcar.site/view.php?q=${blog.title}"><img alt="" class="img-fluid rounded" src="${blog.img1}"></a>
             <div class="card-body px-2">
             <h4 class="text-primary badge bg-main text-light" href="">${blog.category}</h4>
             <h3 class="fw-semibold my-2"><a class="text-decoration-none text-dark oswald" href="">${blog.title}</a></h3>
