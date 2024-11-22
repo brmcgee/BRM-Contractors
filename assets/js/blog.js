7ac1a18e8794c350a7051a8e6b0318b17bb04d30
Brian McGee <92900596+brmcgee@users.noreply.github.com>
2024-11-22 11:40:30 -0500

    Add files via upload

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,9 +1,42 @@
+function el(target) {
+    return document.querySelector(target);
+}
+
+const bigBlogElem = el('#bigBlog');
+const smallBlogElem = el('#smallBlog'); 
+
+async function fetchPostId (postId, target, size){
+    let url = `https://brm.boxcar.site/posts/${postId}`;
+
+    try {
+        
+        let response = await fetch (url);
+        try {
+            let data = await response.json();
+            handleFetchPost(data, target, size)
+        } catch (parseError) {
+            console.log(parseError);
+        }
+    } catch (networkError) {
+        console.log(networkError);
+    }
+}
+
+function handleFetchPost (data, target, size) {
+    if (size == 'big') {
+        el(target).innerHTML = makeBigBlog(data);
+    } else {
+
+        el(target).innerHTML += makeSmallBlog(data)
+    }
+}
+
 
 function makeSmallBlog(blog) {
     let html= `
         <div class="col-6">
         <div class="card border-0">
-            <a href="https://boxcar.site/view.php?q=${blog.title}"><img alt="" class="img-fluid rounded" src="${blog.img1}"></a>
+            <a href="https://boxcar.site/view.php?q=${blog.title}"><img alt="" class="img-fluid rounded" src="${blog.imgs[0]}"></a>
             <div class="card-body px-2">
                 <p class="text-primary badge bg-main text-light" href="">${blog.category}</p>
                 <h5 class="fw-semibold oswald" style="height:70px;"><a class="text-decoration-none text-dark" href="">${blog.title}</a></h5>
@@ -14,14 +47,11 @@ function makeSmallBlog(blog) {
     return html;
 }
 
-
-
-
 function makeBigBlog(blog) {
     let html= `
     
         <div class="card border-0">
-            <a href="https://boxcar.site/view.php?q=${blog.title}"><img alt="" class="img-fluid rounded" src="${blog.img1}"></a>
+            <a href="https://boxcar.site/view.php?q=${blog.title}"><img alt="" class="img-fluid rounded" src="${blog.imgs[0]}"></a>
             <div class="card-body px-2">
             <h4 class="text-primary badge bg-main text-light" href="">${blog.category}</h4>
             <h3 class="fw-semibold my-2"><a class="text-decoration-none text-dark oswald" href="">${blog.title}</a></h3>
@@ -33,8 +63,6 @@ function makeBigBlog(blog) {
     return html;
 };
 
-let bigBlog = document.getElementById('bigBlog');
-// let bigHtmlSegment = makeBigBlog(brm[9]);
-bigBlog.innerHTML = 'content unavailable'
+
 
 
